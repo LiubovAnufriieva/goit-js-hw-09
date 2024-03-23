0899ac1a7f2ab0da7ba636c07da107fd268cf1b9
LiubovAnufriieva <lyuzika@gmail.com>
2024-03-23 20:57:19 +0200

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const textarea = form.querySelector("textarea");
+const button = form.querySelector("button");
+
+
+textarea.addEventListener("input", onTextareaInput);
+form.addEventListener("submit", handleSubmit);
+
+populateTextarea();
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log("send");
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+    const message = event.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if(savedMessage) {
+        textarea.value = savedMessage;
+    }
+}
+
+
 console.log("Form"); 
