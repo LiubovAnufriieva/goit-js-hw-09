4c2b1a5c686f1b6d44686b3cce4161d4f2aee9a7
LiubovAnufriieva <lyuzika@gmail.com>
2024-03-25 15:51:33 +0200

    Fixing form-js according to feedback

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,52 +1,39 @@
-const STORAGE_KEY = "feedback-form-state";
-const form = document.querySelector(".feedback-form");
-const { email, message } = form.elements;
-const localData = localStorage.getItem(STORAGE_KEY);
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+const emailInput = form.querySelector('input');
 
-form.addEventListener("input", onInput);
-form.addEventListener("submit", onSubmit);
+form.addEventListener('submit', onSubmit);
+textarea.addEventListener('input', onTextareaInput);
+emailInput.addEventListener('input', onTextareaInput);
 
-checkLocalData();
-
-function checkLocalData() {
-    if (localData) {
-        email.value = JSON.parse(localData).email;
-        message.value = JSON.parse(localData).message;
-    }
-    else {
-        email.value = "";
-        message.value = "";
-    }
-};
-
-function onInput(event) {
-    const message = event.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
+function updateFormFields() {
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  emailInput.value = formData.email ?? '';
+  textarea.value = formData.message ?? '';
 }
 
-// function onInput() {
-//     const formData = {
-//         email: email.value.trim(),
-//         message: message.value.trim()
-//     }
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
-// };
+updateFormFields();
+
+function onTextareaInput(event) {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value.trim(),
+    message: textarea.value.trim(),
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
 function onSubmit(event) {
-    event.preventDefault();
-    if(!email.value) {
-        alert("You forget to add your email!")
-        return
-    }
-    if(!message.value) {
-        alert("You forget to add your message!")
-        return
-    } else {
-        console.log(localStorage.getItem(STORAGE_KEY));
-        localStorage.removeItem(STORAGE_KEY);
-        evt.target.reset()
-    }};
+  event.preventDefault();
 
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  
+  if(!textarea.value || !emailInput.value) {
+    return 
+  }
+  console.log('send');
 
-
-
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
